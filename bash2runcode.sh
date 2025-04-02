ed39f9f1b177d0e3d3dbc800c6d2b31c08f2fa2f
ana-caznok <a231745@dac.unicamp.br>
2025-04-01 18:23:42 -0600

    changed bash

--- bash2runcode.sh
+++ bash2runcode.sh
@@ -4,4 +4,6 @@ export DATA_PATH="/media/ana-caznok/SSD-08/icasp_4090/icasp/data/Link_2"
 #python train_seg_rec.py --config configs/test.yaml
 #python train_seg_rec.py --config configs/test_double.yaml
 
-python train_seg_rec.py --config configs/test_ifft.yaml
+python train_seg_rec.py --config configs/fft2ifft2hsi.yaml
+python train_seg_rec.py --config configs/fft2hsi.yaml
+python train_seg_rec.py --config configs/fft2fft.yaml
