40e0bdfe4cf4e0c97f3880cfc8def5ff6e9dca9b
ana-caznok <a231745@dac.unicamp.br>
2025-04-14 13:38:09 -0600

    now making it for 1000 epochs

---
+++ slurm/msi2hsi_1000_mraessimsam.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# ====================================
+#SBATCH --job-name=msi2hsi1000
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --partition=gpu-v100
+#SBATCH --gres=gpu:1
+#SBATCH --mem=7GB
+#SBATCH --time=0-24:00:00
+#SBATCH --output=msi2hsi_1000_mraessimsam.out
+# ====================================
+# Activate Conda and then the environment.
+
+source ~/software/init-conda
+conda activate agrvai
+export CODE_PATH="/home/ana.caznoksilveira/recon-segment"
+export DATA_PATH="/home/ana.caznoksilveira/icasp/icasp/data/Link_2/downsampled"
+export WANDB_API_KEY=91726ad327981a84eb14736bd7e3800221958491
+
+wandb login
+cd /home/ana.caznoksilveira/recon-segment
+
+
+python train_seg_rec.py --config configs/msi2hsi_1000_mraessimsam.yaml
