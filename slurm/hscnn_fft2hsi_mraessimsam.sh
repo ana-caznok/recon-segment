10882e5e1bdb1793f4584e272415eacaa279f69d
ana-caznok <a231745@dac.unicamp.br>
2025-04-29 13:35:39 -0600

    added hscnn slurm

---
+++ slurm/hscnn_fft2hsi_mraessimsam.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# ====================================
+#SBATCH --job-name=hscnnfft
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=gpu-v100
+#SBATCH --gres=gpu:1
+#SBATCH --mem=25GB
+#SBATCH --time=0-24:00:00
+#SBATCH --output=hscnn_fft2hsi.out
+# ====================================
+# Activate Conda and then the environment.
+
+source ~/software/init-conda
+conda activate agrvai
+export CODE_PATH="/home/ana.caznoksilveira/recon-segment"
+export DATA_PATH="/home/ana.caznoksilveira/icasp/icasp/data/Link_2/downsampled"
+export WANDB_API_KEY=91726ad327981a84eb14736bd7e3800221958491
+
+wandb login
+cd /home/ana.caznoksilveira/recon-segment
+
+
+python train_seg_rec.py --config configs/hscnn_fft2hsi_mraessimsam.yaml
